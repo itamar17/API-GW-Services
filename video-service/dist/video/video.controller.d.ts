6c3295de3b0dab27297aa68ae395e4b43c24025c
itamar <itamar@fireblocks.com>
2023-02-13 14:47:01 +0200

    Added user-service & video-service

--- video-service/dist/video/video.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Video, VideoService } from './video.service';
-export declare class VideoController {
-    private readonly videoService;
-    constructor(videoService: VideoService);
-    getAllVideos(): Promise<Video[]>;
-    getVideoById(id: string): Promise<Video>;
-}
